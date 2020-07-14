5546b4f8bc15b0f6932d45e55d0cfd43a2a0eaf7
Kiri Um <kirirotha@gmail.com>
2020-07-14 18:59:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,39 @@
+
+
+class Dog 
+  def name 
+    @name
+  end
+  
+  def name= (dog_name) 
+    @name = dog_name
+  end
+  
+  def breed 
+    @breed
+  end
+  
+  def breed= (dog_breed)
+    @breed = dog_breed
+  end
+end
+
+class Person
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+end
+
+
+
